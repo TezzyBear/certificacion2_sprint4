5acd41cad627f8cd9c2735901d389a189370bf82
Renzo Vargas <teztortsg@gmail.com>
2020-09-17 16:30:55 -0500

    correcciones

--- static/js/press_instructions.js
+++ static/js/press_instructions.js
@@ -1,28 +1,6 @@
 function mouseUp() {    
-    $('#instrucciones').hide();
+    $('#modalInstrucciones').modal('hide');
   }
   function mouseDown() {
-    $('#instrucciones').show();
+    $('#modalInstrucciones').modal('show');
   }
-
-  $(function() {
-    $('button#on').bind('click', function() {
-      $(this).toggleClass('btn-danger btn-secondary');
-      var text=$('button#on').text();
-      if(text === "Iniciar"){
-        $(this).html('Detener');
-        $.getJSON('/startRecording',
-          function(data) {
-        //do nothing
-      });
-      } else{
-        $(this).text('Iniciar');
-        $.getJSON('/stopRecording',
-          function(data) {
-            window.location.replace("http://localhost:5001/details")
-        //go to details view
-      });
-      }
-      return false;
-    });
-  });
